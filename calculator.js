e9a8bb81e0d5ab5b339226b3a2ca3678b3e28f89
hellosamm <142949327+hellosamm@users.noreply.github.com>
2023-10-31 15:54:27 -1000

    number and operation buttons record values and console.log()

--- calculator.js
+++ calculator.js
@@ -1,8 +1,10 @@
+// functions for the 4 main operators -> add, subtract, multiply and divide
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
+// function that takes in 2 number values (displayValue), as well as the operator value
 function operate(a, operator, b) {
   switch (operator) {
     case "+":
@@ -16,7 +18,34 @@ function operate(a, operator, b) {
   }
 }
 
+// declaring the clear button, num buttons, screen and operator buttons
 const clearBtn = document.getElementById("clearButton");
+const numBtn = document.getElementsByClassName("num");
+const screen = document.getElementById("screen");
+const operatorBtn = document.getElementsByClassName("operator");
 
-const numBtns = document.getElementsByClassName("num");
-numBtns.addEventListener("click", showInDisplay);
+// event listener for the clear button -> currently it console logs a message
+clearBtn.addEventListener("click", function () {
+  console.log("clear the math");
+});
+
+// assigning empty values to store the operator value and the displayValue (the number selected)
+let operator = "";
+let displayValue = "";
+
+// event listener for number buttons -> stores the value as displayValue, updates the screen and console logs the value
+for (let i = 0; i < numBtn.length; i++) {
+  numBtn[i].addEventListener("click", function () {
+    displayValue = numBtn[i].textContent;
+    screen.textContent = numBtn[i].textContent;
+    console.log(numBtn[i].textContent);
+  });
+}
+
+// event listener for operator buttons -> stores the value as operator, console logs the value
+for (let i = 0; i < operatorBtn.length; i++) {
+  operatorBtn[i].addEventListener("click", function () {
+    operator = operatorBtn[i].textContent;
+    console.log(operatorBtn[i].textContent);
+  });
+}
