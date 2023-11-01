9388f981f0074f37118855537e788a4ef1327c74
hellosamm <142949327+hellosamm@users.noreply.github.com>
2023-10-31 14:39:52 -1000

    buttons made, starting on js

--- calculator.js
+++ calculator.js
@@ -1,1 +1,22 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
 
+function operate(a, operator, b) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+  }
+}
+
+const clearBtn = document.getElementById("clearButton");
+
+const numBtns = document.getElementsByClassName("num");
+numBtns.addEventListener("click", showInDisplay);
